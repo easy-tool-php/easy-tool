f3d6b840d132fef0d4ee87dc7c94842e0e9d436f
Zengliwei <zengliwei@163.com>
2022-01-07 10:20:13 +0800

    Create di.php

---
+++ app/di.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Psr\Http\Message\RequestInterface' => 'EasyTool\Framework\App\Http\Message\Request',
+    'Psr\Http\Message\RequestFactoryInterface' => 'EasyTool\Framework\App\Http\Message\Request\Factory',
+    'Psr\Http\Message\ResponseInterface' => 'EasyTool\Framework\App\Http\Message\Response',
+    'Psr\Http\Message\ResponseFactoryInterface' => 'EasyTool\Framework\App\Http\Message\Response\Factory',
+    'Psr\Http\Message\StreamInterface' => 'EasyTool\Framework\App\Http\Message\Stream',
+    'Psr\Http\Message\StreamFactoryInterface' => 'EasyTool\Framework\App\Http\Message\Stream\Factory',
+    'Psr\Http\Message\UriInterface' => 'EasyTool\Framework\App\Http\Message\Uri',
+    'Psr\Http\Message\UriFactoryInterface' => 'EasyTool\Framework\App\Http\Message\Uri\Factory',
+    'Psr\Http\Message\ServerRequestInterface' => 'EasyTool\Framework\App\Http\Server\Request',
+    'Psr\Http\Server\RequestHandlerInterface' => 'EasyTool\Framework\App\Http\Server\Request\Handler'
+];
