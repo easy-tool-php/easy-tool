b8303eca3aaa98cbfa1af9a9bb63c25e93237a79
Zengliwei <zengliwei@163.com>
2022-01-08 11:19:42 +0800

    Create env.php

---
+++ app/env.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'default' => [
+            'driver' => 'Pdo_Mysql',
+            'host' => 'easy_tool_mysql',
+            'database' => 'project',
+            'username' => 'project',
+            'password' => 'project'
+        ]
+    ]
+];
