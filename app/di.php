0d24ed2abbfa69edce50e59f33101f13d4e50b4f
Zengliwei <zengliwei@163.com>
2022-01-20 15:14:26 +0800

    Update di.php

--- app/di.php
+++ app/di.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'Psr\Http\Message\RequestInterface' => 'EasyTool\Framework\App\Http\Message\Request',
-    'Psr\Http\Message\RequestFactoryInterface' => 'EasyTool\Framework\App\Http\Message\Request\Factory',
-    'Psr\Http\Message\ResponseInterface' => 'EasyTool\Framework\App\Http\Message\Response',
-    'Psr\Http\Message\ResponseFactoryInterface' => 'EasyTool\Framework\App\Http\Message\Response\Factory',
-    'Psr\Http\Message\StreamInterface' => 'EasyTool\Framework\App\Http\Message\Stream',
-    'Psr\Http\Message\StreamFactoryInterface' => 'EasyTool\Framework\App\Http\Message\Stream\Factory',
-    'Psr\Http\Message\UriInterface' => 'EasyTool\Framework\App\Http\Message\Uri',
-    'Psr\Http\Message\UriFactoryInterface' => 'EasyTool\Framework\App\Http\Message\Uri\Factory',
-    'Psr\Http\Message\ServerRequestInterface' => 'EasyTool\Framework\App\Http\Server\Request',
-    'Psr\Http\Server\RequestHandlerInterface' => 'EasyTool\Framework\App\Http\Server\Request\Handler'
+    'Psr\Http\Message\RequestInterface'         => 'EasyTool\Framework\Http\Message\Request',
+    'Psr\Http\Message\ResponseInterface'        => 'EasyTool\Framework\Http\Message\Response',
+    'Psr\Http\Message\StreamInterface'          => 'EasyTool\Framework\Http\Message\Stream',
+    'Psr\Http\Message\UriInterface'             => 'EasyTool\Framework\Http\Message\Uri',
+    'Psr\Http\Message\RequestFactoryInterface'  => 'EasyTool\Framework\App\Http\Message\RequestFactory',
+    'Psr\Http\Message\ResponseFactoryInterface' => 'EasyTool\Framework\App\Http\Message\ResponseFactory',
+    'Psr\Http\Message\StreamFactoryInterface'   => 'EasyTool\Framework\App\Http\Message\StreamFactory',
+    'Psr\Http\Message\UriFactoryInterface'      => 'EasyTool\Framework\App\Http\Message\UriFactory',
+    'Psr\Http\Message\ServerRequestInterface'   => 'EasyTool\Framework\App\Http\Server\Request',
+    'Psr\Http\Server\RequestHandlerInterface'   => 'EasyTool\Framework\App\Http\Server\Request\Handler'
 ];
