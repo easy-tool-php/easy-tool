af6738d39720cdeb2eb89d45125a18598158f569
Zengliwei <zengliwei@163.com>
2022-01-27 11:51:13 +0800

    Delete events.php

--- app/events.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'after_modules_init' => [
-        ['listener' => 'EasyTool\Framework\App\Listener\CollectDependencyInjections'],
-        ['listener' => 'EasyTool\Framework\App\Listener\CollectEvents'],
-        ['listener' => 'EasyTool\Framework\App\Listener\CollectApiRoutes']
-    ]
-];
